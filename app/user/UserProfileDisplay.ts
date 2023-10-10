ec11ee086e097ebefe56f4e3dc0f6327086d840e
Sim Young Jae <hanul@hanul.me>
2023-10-10 17:02:28 +0900

    dev

--- app/user/UserProfileDisplay.ts
+++ app/user/UserProfileDisplay.ts
@@ -1,4 +1,4 @@
-import { DomNode, el } from "common-dapp-module";
+import { DomNode, el, MaterialIcon } from "common-dapp-module";
 import UserDetails from "../database-interface/UserDetails.js";
 
 export default class UserProfileDisplay extends DomNode {
@@ -10,7 +10,11 @@ export default class UserProfileDisplay extends DomNode {
         el(
           ".profile-image-wrapper",
           el(".profile-image", {
-            style: { backgroundImage: `url(${userDetails.profile_image})` },
+            style: {
+              backgroundImage: `url(${
+                userDetails.profile_image?.replace("_normal", "")
+              })`,
+            },
           }),
         ),
         el("h2", userDetails.display_name),
@@ -19,6 +23,16 @@ export default class UserProfileDisplay extends DomNode {
           el(
             "a",
             "@" + userDetails.x_username,
+            {
+              href: `https://x.com/${userDetails.x_username}`,
+              target: "_blank",
+            },
+          ),
+        ),
+        el(
+          ".socials",
+          el(
+            "a",
             el("img.x-symbol", { src: "/images/x-symbol.svg" }),
             {
               href: `https://x.com/${userDetails.x_username}`,
@@ -26,6 +40,35 @@ export default class UserProfileDisplay extends DomNode {
             },
           ),
         ),
+      ),
+      el(
+        ".trading-metrics",
+        el(
+          "section.trading-volume",
+          el(".icon-container", new MaterialIcon("analytics")),
+          el(".metric", el("h3", "Volume"), el(".value", "0.00 X")),
+        ),
+        el(
+          "section.earned",
+          el(".icon-container", new MaterialIcon("savings")),
+          el(".metric", el("h3", "Fees Earned"), el(".value", "0.00 X")),
+        ),
+        el(
+          "section.holdings",
+          el(".icon-container", new MaterialIcon("account_balance_wallet")),
+          el(".metric", el("h3", "Holdings"), el(".value", "0")),
+        ),
+        el(
+          "section.price",
+          el(".icon-container", new MaterialIcon("sell")),
+          el(".metric", el("h3", "Price"), el(".value", "0.00 X")),
+        ),
+      ),
+      el(
+        ".social-metrics",
+        el("section.holders", el("span.value", "0"), el("span", " Holders")),
+        el("section.following", el("span.value", "0"), el("span", " Following")),
+        el("section.followers", el("span.value", "0"), el("span", " Followers")),
       ),
     );
   }
