85ffc9d4f23dbd819de73e0ab4306d8882be26f0
Sim Young Jae <hanul@hanul.me>
2023-10-10 16:19:47 +0900

    dev

--- app/user/UserProfileDisplay.ts
+++ app/user/UserProfileDisplay.ts
@@ -1,8 +1,32 @@
-import { DomNode } from "common-dapp-module";
+import { DomNode, el } from "common-dapp-module";
 import UserDetails from "../database-interface/UserDetails.js";
 
 export default class UserProfileDisplay extends DomNode {
   constructor(userDetails: UserDetails) {
     super(".user-profile-display");
+    this.append(
+      el(
+        ".profile",
+        el(
+          ".profile-image-wrapper",
+          el(".profile-image", {
+            style: { backgroundImage: `url(${userDetails.profile_image})` },
+          }),
+        ),
+        el("h2", userDetails.display_name),
+        el(
+          "h3",
+          el(
+            "a",
+            "@" + userDetails.x_username,
+            el("img.x-symbol", { src: "/images/x-symbol.svg" }),
+            {
+              href: `https://x.com/${userDetails.x_username}`,
+              target: "_blank",
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
