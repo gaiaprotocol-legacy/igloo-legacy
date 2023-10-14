d58d565837e5e0a411912cc116219fb6dc72c634
Sim Young Jae <hanul@hanul.me>
2023-10-14 22:46:07 +0900

    dev

---
+++ database-design/functions/increase_subject_total_trading_fees_earned.sql
@@ -0,0 +1,18 @@
+begin
+    update subject_details
+    set
+        total_earned_trading_fees = total_earned_trading_fees + new.args[7]::numeric
+    where
+        subject = new.args[1];
+    update subject_key_holders
+    set
+        total_earned_trading_fees = total_earned_trading_fees + new.args[7]::numeric
+    where
+        subject = new.args[1] and wallet_address = new.args[2];
+    update user_details
+    set
+        total_earned_trading_fees = total_earned_trading_fees + new.args[7]::numeric
+    where
+        wallet_address = new.args[2];
+    return null;
+end;
