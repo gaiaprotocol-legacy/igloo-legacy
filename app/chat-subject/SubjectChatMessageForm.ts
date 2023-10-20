c94dff8c32c5bc968780199a852e857e4b39d735
Sim Young Jae <hanul@hanul.me>
2023-10-20 16:01:56 +0900

    dev

--- app/chat-subject/SubjectChatMessageForm.ts
+++ app/chat-subject/SubjectChatMessageForm.ts
@@ -1,13 +1,29 @@
 import ChatMessageForm from "../chat/ChatMessageForm.js";
+import { MessageType } from "../database-interface/ChatMessage.js";
+import SubjectChatMessage from "../database-interface/SubjectChatMessage.js";
 import SubjectChatMessageList from "./SubjectChatMessageList.js";
+import SubjectChatService from "./SubjectChatService.js";
 
 export default class SubjectChatMessageForm extends ChatMessageForm {
   constructor(private messageList: SubjectChatMessageList) {
     super(".subject-chat-message-form");
   }
 
-  protected sendMessage(message: string): void {
-    throw new Error("Method not implemented.");
+  protected async sendMessage(message: string) {
+    const optimistic: SubjectChatMessage = {
+      subject: this.messageList.subject,
+      ...this.getOptimisticData(MessageType.MESSAGE, message),
+    };
+
+    const item = this.messageList.addMessage(optimistic).wait();
+    const messageId = await SubjectChatService.sendMessage(
+      this.messageList.subject,
+      message,
+    );
+
+    this.messageList.findMessageItem(messageId)?.delete();
+    item.message.id = messageId;
+    item.done();
   }
 
   protected upload(file: File): void {
--- app/chat-subject/SubjectChatService.ts
+++ app/chat-subject/SubjectChatService.ts
@@ -1,15 +1,28 @@
 import { Supabase } from "common-dapp-module";
+import { MessageType } from "../database-interface/ChatMessage.js";
+import SubjectChatMessage from "../database-interface/SubjectChatMessage.js";
 
 class SubjectChatService {
+  private static readonly FETCH_MESSAGE_LIMIT = 100;
+
   public async sendMessage(subject: string, message: string) {
     const { data, error } = await Supabase.client.from("subject_chat_messages")
-      .insert({ subject, message }).select().single();
+      .insert({ subject, message_type: MessageType.MESSAGE, message }).select()
+      .single();
     if (error) throw error;
     return data.id;
   }
 
   public async upload(subject: string, file: File) {
     //TODO:
+  }
+
+  public async fetchLatestMessages(topic: string) {
+    const { data, error } = await Supabase.client.from("subject_chat_messages")
+      .select().eq("topic", topic).order("created_at", { ascending: false })
+      .limit(SubjectChatService.FETCH_MESSAGE_LIMIT);
+    if (error) throw error;
+    return data as SubjectChatMessage[];
   }
 }
 
