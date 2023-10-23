f710d0944865c41e0f1eb708a40e8c1c8cd9ad1f
Sim Young Jae <hanul@hanul.me>
2023-10-23 14:11:41 +0900

    dev

--- app/user/UsetListItem.ts
+++ app/user/UsetListItem.ts
@@ -1,5 +1,7 @@
-import { DomNode, el, Router } from "common-dapp-module";
+import { DomNode, el, MaterialIcon, Router } from "common-dapp-module";
+import { ethers } from "ethers";
 import UserDetails from "../database-interface/UserDetails.js";
+import SubjectDetailsCacher from "../subject/SubjectDetailsCacher.js";
 import UserDetailsCacher from "./UserDetailsCacher.js";
 
 export default class UserListItem extends DomNode {
@@ -18,16 +20,53 @@ export default class UserListItem extends DomNode {
   }
 
   private render() {
+    const subjectDetails = this.userDetails.wallet_address
+      ? SubjectDetailsCacher.get(this.userDetails.wallet_address)
+      : undefined;
+
     this.empty().append(
-      el(".profile-image", {
-        style: { backgroundImage: `url(${this.userDetails.profile_image})` },
-      }),
       el(
-        ".info",
-        el(".name", this.userDetails.display_name),
-        this.userDetails.x_username
-          ? el(".x-username", `@${this.userDetails.x_username}`)
-          : undefined,
+        ".info-container",
+        el(".profile-image", {
+          style: { backgroundImage: `url(${this.userDetails.profile_image})` },
+        }),
+        el(
+          ".info",
+          el(".name", this.userDetails.display_name),
+          this.userDetails.x_username
+            ? el(".x-username", `@${this.userDetails.x_username}`)
+            : undefined,
+        ),
+      ),
+      el(
+        ".metric-container",
+        el(
+          "section.price",
+          el(".icon-container", new MaterialIcon("group")),
+          el(
+            ".metric",
+            el("h3", "Holders"),
+            el(
+              ".value",
+              subjectDetails ? String(subjectDetails.key_holder_count) : "0",
+            ),
+          ),
+        ),
+        el(
+          "section.price",
+          el(".icon-container", new MaterialIcon("sell")),
+          el(
+            ".metric",
+            el("h3", "Price"),
+            el(
+              ".value",
+              subjectDetails
+                ? ethers.formatEther(subjectDetails.last_fetched_key_price)
+                : "0",
+              el("img.avax-symbol", { src: "/images/avax-symbol.svg" }),
+            ),
+          ),
+        ),
       ),
     );
   }
