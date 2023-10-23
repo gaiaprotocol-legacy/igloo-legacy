d2d90ac70249168dee11d44c152dff4caee46ba5
Sim Young Jae <hanul@hanul.me>
2023-10-23 21:56:13 +0900

    dev

---
+++ app/database-interface/Notification.ts
@@ -0,0 +1,20 @@
+export enum Notificationype {
+  BUY_KEY,
+  SELL_KEY,
+  FOLLOW,
+  LIKE,
+  REPOST,
+  COMMENT,
+  TAG,
+}
+
+export default interface Notification {
+  id: number;
+  user_id: string;
+  triggered_by: string;
+  type: number;
+  source_id?: number;
+  amount?: number;
+  read_at?: string;
+  created_at: string;
+}
