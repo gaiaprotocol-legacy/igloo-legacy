7211414322ec682ac2e65ffc7df10d466b7b26a1
Sim Young Jae <hanul@hanul.me>
2023-10-26 19:01:31 +0900

    Create PostForm.ts

---
+++ app/post/PostForm.ts
@@ -0,0 +1,13 @@
+import { DomNode } from "common-dapp-module";
+
+export default class PostForm extends DomNode {
+  private waitingUploadFiles: File[] = [];
+
+  private addUploadFile(file: File) {
+    //TODO:
+  }
+
+  private deleteUploadFile(index: number) {
+    //TODO:
+  }
+}
