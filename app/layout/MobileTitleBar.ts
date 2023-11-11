757a1dc1c21c8e3094cbe8051c86ecddf5e6bf51
yjgaia <hanul@hanul.me>
2023-11-12 02:14:46 +0900

    dev

--- app/layout/MobileTitleBar.ts
+++ app/layout/MobileTitleBar.ts
@@ -1,11 +1,74 @@
-import { DomNode } from "common-app-module";
+import { DomNode, el, msg, Router, StringUtil } from "common-app-module";
+import MaterialIcon from "../MaterialIcon.js";
+import SignedUserManager from "../user/SignedUserManager.js";
 
 export default class MobileTitleBar extends DomNode {
+  private welcomeMessage: string;
+
+  private logo: DomNode;
+  private backButton: DomNode;
+  private titleDisplay: DomNode;
+
   constructor() {
     super(".mobile-title-bar");
+
+    this.welcomeMessage = SignedUserManager.signed
+      ? msg("mobile-title-bar-welcome-message-signed-in", {
+        name: SignedUserManager.user?.display_name,
+      })
+      : msg("mobile-title-bar-welcome-message-not-signed-in");
+
+    this.append(
+      this.logo = el("h1", el("img", { src: "/images/logo.png" }), {
+        click: () => Router.go("/"),
+      }),
+      this.backButton = el("button.back", new MaterialIcon("arrow_back"), {
+        click: () => history.back(),
+      }),
+      this.titleDisplay = el("p", this.welcomeMessage),
+      !SignedUserManager.signed
+        ? el("button.login", new MaterialIcon("login"), {
+          click: () => SignedUserManager.signIn(),
+        })
+        : el("a.signed-user", {
+          style: {
+            backgroundImage:
+              `url(${SignedUserManager.user?.profile_image_thumbnail})`,
+          },
+          click: () => Router.go("/profile"),
+        }),
+    );
+  }
+
+  private showLogo() {
+    this.logo.deleteClass("hidden");
+    this.backButton.addClass("hidden");
+  }
+
+  private showBackButton() {
+    this.logo.addClass("hidden");
+    this.backButton.deleteClass("hidden");
   }
 
   public changeTitle(uri: string) {
-    //TODO:
+    if (
+      uri === "" || uri === "chats" || uri.startsWith("chats/") ||
+      uri === "explore" || uri === "search" || uri === "notifications" ||
+      uri === "settings"
+    ) {
+      this.titleDisplay.text = uri === ""
+        ? this.welcomeMessage
+        : StringUtil.toTitleCase(uri);
+      this.showLogo();
+    } else if (uri.startsWith("post/")) {
+      this.titleDisplay.text = "Post";
+      this.showBackButton();
+    } else { // x username
+      const xUsername = uri.split("/")[0];
+      const path = uri.split("/")[1];
+      this.titleDisplay.text = "@" + xUsername +
+        (path ? "'s " + StringUtil.toTitleCase(path) : "");
+      this.showBackButton();
+    }
   }
 }
