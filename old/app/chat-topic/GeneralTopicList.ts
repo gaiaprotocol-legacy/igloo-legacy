a30ebb0b2cc1f7c201a959872e203e744b74cc01
yjgaia <hanul@hanul.me>
2023-11-22 17:27:24 +0900

    dev

--- old/app/chat-topic/GeneralTopicList.ts
+++
@@ -1,8 +0,0 @@
-import TopicList from "./TopicList.js";
-
-export default class GeneralTopicList extends TopicList {
-  constructor() {
-    super(".general-topic-list", "No general topics yet.");
-    this.addTopic("general");
-  }
-}
