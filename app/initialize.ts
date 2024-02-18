6bba84ee0f0aa5c7300de4a497d0bb306aaaca47
yjgaia <hanul@hanul.me>
2024-02-18 18:26:47 +0900

    dev

--- app/initialize.ts
+++ app/initialize.ts
@@ -8,53 +8,24 @@ import {
 } from "@common-module/app";
 import {
   BlockTimeManager,
-  ContractType,
-  CreatorChatRoomView,
-  CreatorsView,
   Env,
-  FSESFLayout,
-  FSESFSignedUserManager,
-  GroupChatRoomView,
-  GroupsView,
-  HashtagChatRoomView,
-  HashtagsView,
-  inject_fsesf_msg,
-  MyCreatorsView,
-  MyGroupsView,
-  MyPointsView,
-  NewCreatorsView,
-  NewGroupsView,
-  PointLeaderboardView,
-  PointsView,
-  SettingsView,
-  TopCreatorsView,
-  TopGroupsView,
-  TrendingCreatorsView,
-  TrendingGroupsView,
+  HESFLayout,
+  HESFSignedUserManager,
+  inject_hesf_msg,
   WalletManager,
 } from "hesf";
-import { fantom, fantomSonicTestnet, fantomTestnet } from "viem/chains";
+import { avalanche, avalancheFuji } from "viem/chains";
 import AboutView from "./AboutView.js";
 import Config from "./Config.js";
 
-inject_fsesf_msg();
+inject_hesf_msg();
 
 MaterialIconSystem.launch();
 
 export default async function initialize(config: Config) {
-  Env.domain = "thunderdome.so";
-  Env.keyName = "ticket";
-  Env.blockchain = {
-    ...config.blockchain,
-    symbolDisplay: "FTM",
-  };
-  Env.contractAddresses = {
-    [ContractType.CreatorKeys]: "0x298c92D5af8eEFA02b55dE45cb2337704af1b894",
-    [ContractType.GroupKeys]: "0xe741b5DF37FB86eaB58F616dA0f4BfF10251C37a",
-    [ContractType.HashtagKeys]: "0x23e0035F44cB5Bb4fb83e3F4CA413DB39c6f7BF0",
-  };
-  Env.messageForWalletLinking = "Link Wallet to Thunder Dome";
-  Env.defaultHashtag = "thunderdome";
+  Env.domain = "igloo.ax";
+  Env.keyName = "ice";
+  Env.messageForWalletLinking = "Link Wallet to Igloo";
 
   AppInitializer.initialize(
     config.supabaseUrl,
@@ -63,57 +34,17 @@ export default async function initialize(config: Config) {
   );
 
   WalletManager.init(config.walletConnectProjectId, [
-    fantom,
-    fantomTestnet,
-    fantomSonicTestnet,
+    config.dev ? avalancheFuji : avalanche,
   ]);
 
   await SplashLoader.load(el("img", { src: "/images/logo-transparent.png" }), [
-    FSESFSignedUserManager.fetchUserOnInit(),
-    BlockTimeManager.init(0.3),
+    HESFSignedUserManager.fetchUserOnInit(),
+    BlockTimeManager.init(),
   ]);
 
-  Router.route("**", FSESFLayout);
+  Router.route("**", HESFLayout);
 
   Router.route(["", "about"], AboutView);
-
-  Router.route([
-    "creators",
-    "creator/{creatorAddress}",
-    "creators/trending",
-    "creators/top",
-    "creators/new",
-  ], CreatorsView);
-  Router.route(["creators", "creator/{creatorAddress}"], MyCreatorsView);
-  Router.route(["creators", "creator/{creatorAddress}"], CreatorChatRoomView);
-  Router.route("creators/trending", TrendingCreatorsView);
-  Router.route("creators/top", TopCreatorsView);
-  Router.route("creators/new", NewCreatorsView);
-
-  Router.route(
-    [
-      "groups",
-      "group/{groupId}",
-      "groups/trending",
-      "groups/top",
-      "groups/new",
-    ],
-    GroupsView,
-  );
-  Router.route(["groups", "group/{groupId}"], MyGroupsView);
-  Router.route(["groups", "group/{groupId}"], GroupChatRoomView);
-  Router.route("groups/trending", TrendingGroupsView);
-  Router.route("groups/top", TopGroupsView);
-  Router.route("groups/new", NewGroupsView);
-
-  Router.route(["hashtags", "hashtag/{hashtag}"], HashtagsView);
-  Router.route(["hashtags", "hashtag/{hashtag}"], HashtagChatRoomView);
-
-  Router.route(["points", "points/leaderboard"], PointsView);
-  Router.route("points", MyPointsView);
-  Router.route("points/leaderboard", PointLeaderboardView);
-
-  Router.route("settings", SettingsView);
 
   AuthUtil.checkEmailAccess();
 }
